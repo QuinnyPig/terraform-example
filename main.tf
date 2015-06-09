9d3d697402f0133b519e700cff007350ec9e8549
Corey Quinn <corey@sequestered.net>
2015-06-09 10:45:34 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,102 @@
+# Specify the provider and access details
+provider "aws" {
+    region = "${var.aws_region}"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+}
+
+# Our default security group allows access
+# to the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+    name = "terraform_demo"
+    description = "Used for the demo of Terraform for RMS"
+
+    # SSH access from anywhere
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # HTTP access from anywhere
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # outbound internet access
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_elb" "web" {
+  name = "terraform-example-elb"
+
+  # The same availability zone as our instance
+  availability_zones = ["${aws_instance.web.availability_zone}"]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  # The instance is registered automatically
+  instances = ["${aws_instance.web.id}"]
+}
+
+
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+
+    # The path to your keyfile
+    key_file = "${var.key_path}"
+  }
+
+  instance_type = "m1.small"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair; one can be generated and downloaded
+  # from the AWS console.
+  #
+  # Note that the key file must not have a passphrase assigned.
+  #
+  # https://console.aws.amazon.com/ec2/v2/home?region=us-east-1#KeyPairs:
+  #
+  key_name = "${var.key_name}"
+
+  # Our Security group to allow HTTP and SSH access
+  security_groups = ["${aws_security_group.default.name}"]
+
+  # We run a remote provisioner on the instance after creating it.
+  provisioner "remote-exec" {
+    inline = [
+        "sudo apt-get -y update",
+        "sudo apt-get -y install nginx",
+        "sudo service nginx start",
+        "sudo rm -f /usr/share/nginx/www/*",
+        "sudo chmod 777 /usr/share/nginx/www/"
+    ]
+  }
+
+  provisioner "file" {
+    source = "files/"
+    destination = "/usr/share/nginx/www/"
+  }
+}
